599f383f680ed530c568222c423a4083bb1b04a2
RAM García <magase98@gmail.com>
2020-05-19 21:46:25 +0200

    Ej1-Serializacion 0.1
    Creacion de los numeros aleatorios y las veces que se repite su salida

--- Ficheros_Binarios_Clase/src/ej1/MainEj1.java
+++ Ficheros_Binarios_Clase/src/ej1/MainEj1.java
@@ -1,10 +1,35 @@
 package ej1;
 
+import java.util.*;
+
 public class MainEj1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner teclado = new Scanner (System.in);
+		
+		int cantNumeros;
+		
+		MetodosEj1 metodos = new MetodosEj1();
+		
+		System.out.println("Cuantos numeros quieres que salgan");
+		
+		cantNumeros = teclado.nextInt();
+		
+		for (int i=0; i <= cantNumeros; i++) {
+			
+			int random = (int)(Math.random() * 100);
+			System.out.println(random);
+		}
+		
+		
+		
+		metodos.numerosAleatorios(cantNumeros);
+		
+		
+		
+		
+		teclado.close();
 	}
 
 }
--- Ficheros_Binarios_Clase/src/ej1/MetodosEj1.java
+++ Ficheros_Binarios_Clase/src/ej1/MetodosEj1.java
@@ -2,4 +2,12 @@ package ej1;
 
 public class MetodosEj1 {
 
+	public void numerosAleatorios(int cantNum) {
+		
+		System.out.println(cantNum);		
+		
+	}
+	
+	
+	
 }
