ec9f83f01c14ae2b2d2290075e99a8af8f9b9f9b
RAM García <magase98@gmail.com>
2020-05-19 22:32:23 +0200

    Datos a un archivo
    Intento de integrar los datos generados del array en un archivo de texto

--- Ficheros_Binarios_Clase/src/ej1/MainEj1.java
+++ Ficheros_Binarios_Clase/src/ej1/MainEj1.java
@@ -1,5 +1,6 @@
 package ej1;
 
+import java.io.*;
 import java.util.*;
 
 public class MainEj1 {
@@ -10,18 +11,45 @@ public class MainEj1 {
 		
 		int cantNumeros;
 		
+		
+		ArrayList <Character> numerosGuardados = new ArrayList <Character>();
+		
 		MetodosEj1 metodos = new MetodosEj1();
 		
 		System.out.println("Cuantos numeros quieres que salgan");
 		
 		cantNumeros = teclado.nextInt();
 		
-		for (int i=0; i <= cantNumeros; i++) {
+		try {
+			FileWriter ficheroAleatorio = new FileWriter("C:\\Users\\ram\\Desktop\\leyendo_creando\\aleatorios.txt");
+			//FileInputStream ficheroAleatorio = new FileInputStream("C:\\Users\\ram\\Desktop\\leyendo_creando\\aleatorios.dat");
 			
-			int random = (int)(Math.random() * 100);
-			System.out.println(random);
-		}
+			for (int i=0; i <= cantNumeros; i++) {
+			
+				
+			
+				int random = (int)(Math.random() * 100);
+				System.out.println(random);
+				numerosGuardados.add((char) random);
+				
+				ficheroAleatorio.write((char)random);
+				
+			}
+			/*
+			for (Integer e: numerosGuardados) {
+				
+				System.out.println(e);
+				
+			}
+			*/
 		
+		
+		ficheroAleatorio.close();
+		
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		metodos.numerosAleatorios(cantNumeros);
